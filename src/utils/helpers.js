eb729e19703f8d379741a36cb654db72845fab70
MAnojcharan2610 <manojcharan4600@gmail.com>
2025-03-27 18:42:41 +0530

    roomsupdate

---
+++ src/utils/helpers.js
@@ -0,0 +1,197 @@
+import Room1 from '../assets/room1.jpg'
+import Room2 from '../assets/room 2.jpg'
+import Room3 from '../assets/room3.jpg'
+import Room4 from '../assets/room4.jpg'
+import Room5 from '../assets/room5.jpg'
+import Room6 from '../assets/room6.jpg'
+import Room7 from '../assets/room7.jpg'
+import Room8 from '../assets/room8.jpg'
+import Room9 from '../assets/room9.jpg'
+import Ts1  from '../assets/ts1.jpg'
+import Ka1  from '../assets/ka1.jpg'
+import Ka2  from '../assets/ka2.jpg'
+import Tn1  from '../assets/tn1.jpg'
+import Tn2  from '../assets/tn2.jpg'
+import Ts2  from '../assets/ts2.jpg'
+
+
+
+export const roomsObject=[
+    {
+        roomName:"4a",
+        hotelName:"mitra",
+        isAcAvailable:"Yes",
+        roomCapacity:"4",
+        roomCost:"800rs",
+        checkoutTime:"24hrs",
+        roomImg:Room1,
+        location:"hyderabad",
+        hotelNumber:""
+    },
+    {
+        roomName:"213s",
+        hotelName:"hotelblue",
+        isAcAvailable:"no",
+        roomCapacity:"4",
+        roomCost:"399rs",
+        checkoutTime:"24hrs",
+        roomImg:Room2,
+        location:"chennai",
+        hotelNumber:'',
+    },
+    {
+        roomName:"69a",
+        hotelName:"myuri",
+        isAcAvailable:"yes",
+        roomCapacity:"4",
+        roomCost:"950rs",
+        checkoutTime:"24hrs",
+        roomImg:Room3,
+        location:"banglore",
+        hotelNumber:'',
+    },
+    {
+        roomName:"412b",
+        hotelName:"manasa",
+        isAcAvailable:"yes",
+        roomCapacity:"4",
+        roomCost:"750rs",
+        checkoutTime:"24hrs",
+        roomImg:Room4,
+        location:"hyderabad",
+        hotelNumber:'',
+    },
+    {
+        roomName:"46a",
+        hotelName:"paradise",
+        isAcAvailable:"yes",
+        roomCapacity:"2",
+        roomCost:"550rs",
+        checkoutTime:"24hrs",
+        roomImg:Room5,
+        location:"hyderabad",
+        hotelNumber:'',
+    },
+    {
+        roomName:"20g",
+        hotelName:"majestic",
+        isAcAvailable:"yes",
+        roomCapacity:"2",
+        roomCost:"585rs",
+        checkoutTime:"24hrs",
+        roomImg:Room6,
+        location:"banglore",
+        hotelNumber:'',
+    },
+    {
+        roomName:"16f",
+        hotelName:"haven",
+        isAcAvailable:"no",
+        roomCapacity:"4",
+        roomCost:"410rs",
+        checkoutTime:"24hrs",
+        roomImg:Room7,
+        location:"banglore",
+        hotelNumber:'',
+    },
+    {
+        roomName:"4b",
+        hotelName:"gallery",
+        isAcAvailable:"yes",
+        roomCapacity:"4",
+        roomCost:"810rs",
+        checkoutTime:"24hrs",
+        roomImg:Room8,
+        location:"chennai",
+        hotelNumber:'',
+    },
+    {
+        roomName:"009",
+        hotelName:"the nest",
+        isAcAvailable:"yes",
+        roomCapacity:"2",
+        roomCost:"499rs",
+        checkoutTime:"24hrs",
+        roomImg:Room9,
+        location:"chennai",
+        hotelNumber:'',
+    },
+    
+    
+]
+
+export const travelsObject=[
+    {
+        busName:"jai travels",
+        busNo:"ka01 ju4016",
+        busType:"sleeper",
+        isAcAvailable:"yes",
+        busCost:"750rs",
+        tripTime:"9hrs",
+        route:"banglore to hyderabad",
+        via:"Anantapur - Kurnool - Mahabubnagar",
+        driver:"",
+        busImg:Ka1,
+    },
+    {
+        busName:"bayride",
+        busNo:"ka02 hg3400",
+        busType:"semi sleeper",
+        isAcAvailable:"yes",
+        busCost:"350rs",
+        tripTime:"6hrs",
+        route:"banglore to chennai",
+        via:"Hoskote - Mulbagal - Palamaner — Kanchipuram - Sriperumbudur",
+        driverNo:"",
+        busImg:Ka2,
+    },
+    {
+        busName:"",
+        busNo:"ts02 kg7730",
+        busType:"sleeper",
+        isAcAvailable:"yes",
+        busCost:"720rs",
+        tripTime:"9hrs",
+        route:"hyderabad to chennai",
+        via:"Mahbubnagar - Kurnool - Nandyala - Kadapa - Tirupati ",
+        driverNo:"",
+        busImg:Ts2,
+    },
+    {
+        busName:"jai travels",
+        busNo:"ts01 tu4567",
+        busType:"sleeper",
+        isAcAvailable:"yes",
+        busCost:"740rs",
+        tripTime:"9hrs",
+        route:"hyderabad to banglore",
+        via:"Jedcharla — Kurnool — Ananthapur — Penugonda — Chikkaballapur ",
+        driverNo:"",
+        busImg:Ts1,
+    },
+    {
+        busName:"bayride",
+        busNo:"tn01 hg 3504",
+        busType:"semi sleeper",
+        isAcAvailable:"no",
+        busCost:"299rs",
+        tripTime:"6hrs",
+        route:"chennai to banglore",
+        via:"Sriperumbudur — Kanchipuram — Ranipet — Vellore — Ambur — Vaniyambadi — Krishnagiri — Hosur",
+        driverNo:"",
+        busImg:Tn1,
+    },
+    {
+        busName:"",
+        busNo:"tn02 rd4589",
+        busType:"sleeper",
+        isAcAvailable:"yes",
+        busCost:"730rs",
+        tripTime:"9hrs",
+        route:"chennai to hyderabad",
+        via:"Nellore - Kavali - Ongole - Narasaraopeta - Nalgonda",
+        driverNo:"",
+        busImg:Tn2,
+    },
+
+]
